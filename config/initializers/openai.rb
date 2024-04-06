ba2c4fa178bc65a40b3c722c68002eb9bb51b262
Insomnius <aliana.arib@gmail.com>
2024-04-06 22:37:15 +0700

    feat: adding more command

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 return if ENV['DOCKER_BUILD'] == '1' # if docker build then skip
+return unless ENV['OPENAI_ACCESS_TOKEN']
 
 OpenAI.configure do |config|
   config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
